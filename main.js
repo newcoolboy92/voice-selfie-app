b66ee2ecb5ff7110cac1ba1c12d4ca15fa59a0e5
newcoolboy92 <68487297+newcoolboy92@users.noreply.github.com>
2023-05-23 20:06:14 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.oneresult = function(event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content;
+}
